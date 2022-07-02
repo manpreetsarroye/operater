5baf7522f4dd4d85bf36ea028f45661fd89cf68e
Manpreet Kaur <102292106+manpreetsarroye@users.noreply.github.com>
2022-07-02 16:54:18 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,39 @@
+// meraki1
+console.log(8+2);
+
+let browserType = 'mannu';
+console.log(browserType.length);
+
+var vegetables ={
+    veg1:"potato",
+    veg2:"brinjal",
+    veg3:"bottle gourd"
+ }
+  
+ var vegetable1= vegetables.veg1;
+ var vegetable2= vegetables.veg2;
+ var vegetable3= vegetables.veg3;
+ 
+ 
+ console.log(vegetable1);
+ console.log(vegetable2);
+ console.log(vegetable3);
+  
+ //output:
+ // potato
+ // brinjal
+ // bottle gourd
+
+ 
+
+var x = 5, y = 10, z = 15;
+
+console.log(x = y);
+console.log( x+= 1);
+console.log(x -= 1);
+console.log(x *= 5);
+console.log(x /= 5);
+console.log(x %= 2);
+
+
+
